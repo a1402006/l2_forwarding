cdd6310ff6d277e391595b248ed937553541264a
lior789 <lior789@gmail.com>
2015-03-05 07:07:54 -0800

    -fix forwarding packets on setup

--- adsal_L2_forwarding/src/main/java/org/opendaylight/tutorial/tutorial_L2_forwarding/internal/TutorialL2Forwarding.java
+++ adsal_L2_forwarding/src/main/java/org/opendaylight/tutorial/tutorial_L2_forwarding/internal/TutorialL2Forwarding.java
@@ -17,238 +17,228 @@
 
 package org.opendaylight.tutorial.tutorial_L2_forwarding.internal;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.lang.String;
-import java.util.Map;
 import java.util.HashMap;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleException;
-import org.osgi.framework.FrameworkUtil;
+import org.opendaylight.controller.sal.action.Action;
+import org.opendaylight.controller.sal.action.Output;
 import org.opendaylight.controller.sal.core.ConstructionException;
 import org.opendaylight.controller.sal.core.Node;
 import org.opendaylight.controller.sal.core.NodeConnector;
-import org.opendaylight.controller.sal.flowprogrammer.IFlowProgrammerService;
 import org.opendaylight.controller.sal.flowprogrammer.Flow;
-import org.opendaylight.controller.sal.packet.ARP;
+import org.opendaylight.controller.sal.flowprogrammer.IFlowProgrammerService;
+import org.opendaylight.controller.sal.match.Match;
+import org.opendaylight.controller.sal.match.MatchField;
+import org.opendaylight.controller.sal.match.MatchType;
 import org.opendaylight.controller.sal.packet.BitBufferHelper;
 import org.opendaylight.controller.sal.packet.Ethernet;
-import org.opendaylight.controller.sal.packet.ICMP;
 import org.opendaylight.controller.sal.packet.IDataPacketService;
 import org.opendaylight.controller.sal.packet.IListenDataPacket;
 import org.opendaylight.controller.sal.packet.Packet;
 import org.opendaylight.controller.sal.packet.PacketResult;
 import org.opendaylight.controller.sal.packet.RawPacket;
-import org.opendaylight.controller.sal.action.Action;
-import org.opendaylight.controller.sal.action.Output;
-import org.opendaylight.controller.sal.action.Flood;
-import org.opendaylight.controller.sal.match.Match;
-import org.opendaylight.controller.sal.match.MatchType;
-import org.opendaylight.controller.sal.match.MatchField;
-import org.opendaylight.controller.sal.utils.EtherTypes;
 import org.opendaylight.controller.sal.utils.Status;
-import org.opendaylight.controller.sal.utils.NetUtils;
 import org.opendaylight.controller.switchmanager.ISwitchManager;
-import org.opendaylight.controller.switchmanager.Subnet;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.FrameworkUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TutorialL2Forwarding implements IListenDataPacket {
-    private static final Logger logger = LoggerFactory
-            .getLogger(TutorialL2Forwarding.class);
-    private ISwitchManager switchManager = null;
-    private IFlowProgrammerService programmer = null;
-    private IDataPacketService dataPacketService = null;
-    private Map<Long, NodeConnector> mac_to_port = new HashMap<Long, NodeConnector>();
-    private String function = "hub";
+	private static final Logger logger = LoggerFactory
+			.getLogger(TutorialL2Forwarding.class);
+	private ISwitchManager switchManager = null;
+	private IFlowProgrammerService programmer = null;
+	private IDataPacketService dataPacketService = null;
+	private final Map<Node, Map<Long, NodeConnector>> mac_to_port_per_switch = new HashMap<Node, Map<Long, NodeConnector>>();
+	private final String function = "switch";
 
-    void setDataPacketService(IDataPacketService s) {
-        this.dataPacketService = s;
-    }
+	void setDataPacketService(IDataPacketService s) {
+		this.dataPacketService = s;
+	}
 
-    void unsetDataPacketService(IDataPacketService s) {
-        if (this.dataPacketService == s) {
-            this.dataPacketService = null;
-        }
-    }
+	void unsetDataPacketService(IDataPacketService s) {
+		if (this.dataPacketService == s) {
+			this.dataPacketService = null;
+		}
+	}
 
-    public void setFlowProgrammerService(IFlowProgrammerService s)
-    {
-        this.programmer = s;
-    }
+	public void setFlowProgrammerService(IFlowProgrammerService s) {
+		this.programmer = s;
+	}
 
-    public void unsetFlowProgrammerService(IFlowProgrammerService s) {
-        if (this.programmer == s) {
-            this.programmer = null;
-        }
-    }
+	public void unsetFlowProgrammerService(IFlowProgrammerService s) {
+		if (this.programmer == s) {
+			this.programmer = null;
+		}
+	}
 
-    void setSwitchManager(ISwitchManager s) {
-        logger.debug("SwitchManager set");
-        this.switchManager = s;
-    }
+	void setSwitchManager(ISwitchManager s) {
+		logger.debug("SwitchManager set");
+		this.switchManager = s;
+	}
 
-    void unsetSwitchManager(ISwitchManager s) {
-        if (this.switchManager == s) {
-            logger.debug("SwitchManager removed!");
-            this.switchManager = null;
-        }
-    }
+	void unsetSwitchManager(ISwitchManager s) {
+		if (this.switchManager == s) {
+			logger.debug("SwitchManager removed!");
+			this.switchManager = null;
+		}
+	}
 
-    /**
-     * Function called by the dependency manager when all the required
-     * dependencies are satisfied
-     *
-     */
-    void init() {
-        logger.info("Initialized");
-        // Disabling the SimpleForwarding and ARPHandler bundle to not conflict with this one
-        BundleContext bundleContext = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
-        for(Bundle bundle : bundleContext.getBundles()) {
-            if (bundle.getSymbolicName().contains("simpleforwarding")) {
-                try {
-                    bundle.uninstall();
-                } catch (BundleException e) {
-                    logger.error("Exception in Bundle uninstall "+bundle.getSymbolicName(), e); 
-                }   
-            }   
-        }   
- 
-    }
+	/**
+	 * Function called by the dependency manager when all the required
+	 * dependencies are satisfied
+	 * 
+	 */
+	void init() {
+		logger.info("Initialized");
+		// Disabling the SimpleForwarding and ARPHandler bundle to not conflict
+		// with this one
+		BundleContext bundleContext = FrameworkUtil.getBundle(this.getClass())
+				.getBundleContext();
+		for (Bundle bundle : bundleContext.getBundles()) {
+			if (bundle.getSymbolicName().contains("simpleforwarding")) {
+				try {
+					bundle.uninstall();
+				} catch (BundleException e) {
+					logger.error(
+							"Exception in Bundle uninstall "
+									+ bundle.getSymbolicName(), e);
+				}
+			}
+		}
 
-    /**
-     * Function called by the dependency manager when at least one
-     * dependency become unsatisfied or when the component is shutting
-     * down because for example bundle is being stopped.
-     *
-     */
-    void destroy() {
-    }
+	}
 
-    /**
-     * Function called by dependency manager after "init ()" is called
-     * and after the services provided by the class are registered in
-     * the service registry
-     *
-     */
-    void start() {
-        logger.info("Started");
-    }
+	/**
+	 * Function called by the dependency manager when at least one dependency
+	 * become unsatisfied or when the component is shutting down because for
+	 * example bundle is being stopped.
+	 * 
+	 */
+	void destroy() {
+	}
 
-    /**
-     * Function called by the dependency manager before the services
-     * exported by the component are unregistered, this will be
-     * followed by a "destroy ()" calls
-     *
-     */
-    void stop() {
-        logger.info("Stopped");
-    }
+	/**
+	 * Function called by dependency manager after "init ()" is called and after
+	 * the services provided by the class are registered in the service registry
+	 * 
+	 */
+	void start() {
+		logger.info("Started");
+	}
 
-    private void floodPacket(RawPacket inPkt) {
-        NodeConnector incoming_connector = inPkt.getIncomingNodeConnector();
-        Node incoming_node = incoming_connector.getNode();
+	/**
+	 * Function called by the dependency manager before the services exported by
+	 * the component are unregistered, this will be followed by a "destroy ()"
+	 * calls
+	 * 
+	 */
+	void stop() {
+		logger.info("Stopped");
+	}
 
-        Set<NodeConnector> nodeConnectors =
-                this.switchManager.getUpNodeConnectors(incoming_node);
+	private void floodPacket(RawPacket inPkt) {
+		NodeConnector incoming_connector = inPkt.getIncomingNodeConnector();
+		Node incoming_node = incoming_connector.getNode();
 
-        for (NodeConnector p : nodeConnectors) {
-            if (!p.equals(incoming_connector)) {
-                try {
-                    RawPacket destPkt = new RawPacket(inPkt);
-                    destPkt.setOutgoingNodeConnector(p);
-                    this.dataPacketService.transmitDataPacket(destPkt);
-                } catch (ConstructionException e2) {
-                    continue;
-                }
-            }
-        }
-    }
+		Set<NodeConnector> nodeConnectors = this.switchManager
+				.getUpNodeConnectors(incoming_node);
 
+		for (NodeConnector p : nodeConnectors) {
+			if (!p.equals(incoming_connector)) {
+				sendPacket(inPkt, p);
+			}
+		}
+	}
 
+	@Override
+	public PacketResult receiveDataPacket(RawPacket inPkt) {
+		if (inPkt == null) {
+			return PacketResult.IGNORED;
+		}
+		logger.trace("Received a frame of size: {}",
+				inPkt.getPacketData().length);
 
-    @Override
-    public PacketResult receiveDataPacket(RawPacket inPkt) {
-        if (inPkt == null) {
-            return PacketResult.IGNORED;
-        }
+		Packet formattedPak = this.dataPacketService.decodeDataPacket(inPkt);
+		NodeConnector incoming_connector = inPkt.getIncomingNodeConnector();
+		Node incoming_node = incoming_connector.getNode();
 
-        NodeConnector incoming_connector = inPkt.getIncomingNodeConnector();
+		if (formattedPak instanceof Ethernet) {
+			byte[] srcMAC = ((Ethernet) formattedPak).getSourceMACAddress();
+			byte[] dstMAC = ((Ethernet) formattedPak)
+					.getDestinationMACAddress();
 
-        // Hub implementation
-        if (function.equals("hub")) {
-            floodPacket(inPkt);
-        } else {
-            Packet formattedPak = this.dataPacketService.decodeDataPacket(inPkt);
-            if (!(formattedPak instanceof Ethernet)) {
-                return PacketResult.IGNORED;
-            }
+			// Hub implementation
+			if (function.equals("hub")) {
+				floodPacket(inPkt);
+				return PacketResult.CONSUME;
+			}
 
-            learnSourceMAC(formattedPak, incoming_connector);
-            NodeConnector outgoing_connector = 
-                knowDestinationMAC(formattedPak);
-            if (outgoing_connector == null) {
-                floodPacket(inPkt);
-            } else {
-                if (!programFlow(formattedPak, incoming_connector,
-                            outgoing_connector)) {
-                    return PacketResult.IGNORED;
-                }
-                inPkt.setOutgoingNodeConnector(outgoing_connector);
-                this.dataPacketService.transmitDataPacket(inPkt);
-            }
-        }
-        return PacketResult.CONSUME;
-    }
+			// Switch
+			else {
+				long srcMAC_val = BitBufferHelper.toNumber(srcMAC);
+				long dstMAC_val = BitBufferHelper.toNumber(dstMAC);
 
-    private void learnSourceMAC(Packet formattedPak, NodeConnector incoming_connector) {
-        byte[] srcMAC = ((Ethernet)formattedPak).getSourceMACAddress();
-        long srcMAC_val = BitBufferHelper.toNumber(srcMAC);
-        this.mac_to_port.put(srcMAC_val, incoming_connector);
-    }
+				Match match = new Match();
+				match.setField(new MatchField(MatchType.IN_PORT,
+						incoming_connector));
+				match.setField(new MatchField(MatchType.DL_DST, dstMAC.clone()));
 
-    private NodeConnector knowDestinationMAC(Packet formattedPak) {
-        byte[] dstMAC = ((Ethernet)formattedPak).getDestinationMACAddress();
-        long dstMAC_val = BitBufferHelper.toNumber(dstMAC);
-        return this.mac_to_port.get(dstMAC_val) ;
-    }
+				// Set up the mapping: switch -> src MAC address -> incoming
+				// port
+				if (this.mac_to_port_per_switch.get(incoming_node) == null) {
+					this.mac_to_port_per_switch.put(incoming_node,
+							new HashMap<Long, NodeConnector>());
+				}
+				this.mac_to_port_per_switch.get(incoming_node).put(srcMAC_val,
+						incoming_connector);
 
-    private boolean programFlow(Packet formattedPak, 
-            NodeConnector incoming_connector, 
-            NodeConnector outgoing_connector) {
-        byte[] dstMAC = ((Ethernet)formattedPak).getDestinationMACAddress();
+				NodeConnector dst_connector = this.mac_to_port_per_switch.get(
+						incoming_node).get(dstMAC_val);
 
-        Match match = new Match();
-        match.setField( new MatchField(MatchType.IN_PORT, incoming_connector) );
-        match.setField( new MatchField(MatchType.DL_DST, dstMAC.clone()) );
+				// Do I know the destination MAC?
+				if (dst_connector != null) {
 
-        List<Action> actions = new ArrayList<Action>();
-        actions.add(new Output(outgoing_connector));
+					List<Action> actions = new ArrayList<Action>();
+					actions.add(new Output(dst_connector));
 
-        Flow f = new Flow(match, actions);
-        f.setIdleTimeout((short)5);
+					Flow f = new Flow(match, actions);
+					f.setPriority((short) 1);
 
-        // Modify the flow on the network node
-        Node incoming_node = incoming_connector.getNode();
-        Status status = programmer.addFlow(incoming_node, f);
+					// Modify the flow on the network node
+					Status status = programmer.addFlow(incoming_node, f);
+					if (!status.isSuccess()) {
+						logger.warn(
+								"SDN Plugin failed to program the flow: {}. The failure is: {}",
+								f, status.getDescription());
+						return PacketResult.IGNORED;
+					}
+					logger.debug("Installed flow {} in node {}", f,
+							incoming_node);
+					sendPacket(inPkt, dst_connector);
+				} else
+					floodPacket(inPkt);
+			}
+		}
+		return PacketResult.IGNORED;
+	}
 
-        if (!status.isSuccess()) {
-            logger.warn("SDN Plugin failed to program the flow: {}. The failure is: {}",
-                    f, status.getDescription());
-            return false;
-        } else {
-            return true;
-        }
-    }
+	private void sendPacket(RawPacket inPkt, NodeConnector dst_connector) {
+		RawPacket destPkt;
+		try {
+			destPkt = new RawPacket(inPkt);
+			destPkt.setOutgoingNodeConnector(dst_connector);
+			this.dataPacketService.transmitDataPacket(destPkt);
+		} catch (ConstructionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
